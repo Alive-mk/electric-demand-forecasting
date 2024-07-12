ad232f4fa0e69f70ccf8120995dfb17fc62f55bc
BIT-Ming <2924565971@qq.com>
2024-07-12 15:01:27 +0800

    增加baseline

---
+++ baseline.py
@@ -0,0 +1,20 @@
+# 1. 导入需要用到的相关库
+# 导入 pandas 库，用于数据处理和分析
+import pandas as pd
+# 导入 numpy 库，用于科学计算和多维数组操作
+import numpy as np
+
+# 2. 读取训练集和测试集
+# 使用 read_csv() 函数从文件中读取训练集数据，文件名为 'train.csv'
+train = pd.read_csv('./dataset/train.csv')
+# 使用 read_csv() 函数从文件中读取测试集数据，文件名为 'train.csv'
+test = pd.read_csv('./dataset/test.csv')
+
+# 3. 计算训练数据最近11-20单位时间内对应id的目标均值
+target_mean = train[train['dt']<=20].groupby(['id'])['target'].mean().reset_index()
+
+# 4. 将target_mean作为测试集结果进行合并
+test = test.merge(target_mean, on=['id'], how='left')
+
+# 5. 保存结果文件到本地
+test[['id','dt','target']].to_csv('submit.csv', index=None)
