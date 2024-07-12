6fc4e6b612ecadee4523b1919c37a5933725f632
BIT-Ming <2924565971@qq.com>
2024-07-12 15:22:29 +0800

    baseline理解

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # electric-demand-forecasting
-datawhale暑期AI夏令营机器学习方向
+datawhale暑期AI夏令营机器学习方向  
+## baseline部分  
+reset_index()  重新设置了序列号
+![alt text](image.png)  
+如果不使用这个为  
+![alt text](image-1.png)  
+但对最后结果好像不影响  
+  
+  
--- baseline.py
+++ baseline.py
@@ -12,9 +12,9 @@ test = pd.read_csv('./dataset/test.csv')
 
 # 3. 计算训练数据最近11-20单位时间内对应id的目标均值
 target_mean = train[train['dt']<=20].groupby(['id'])['target'].mean().reset_index()
-
+print(target_mean)
 # 4. 将target_mean作为测试集结果进行合并
 test = test.merge(target_mean, on=['id'], how='left')
-
+print(test)
 # 5. 保存结果文件到本地
 test[['id','dt','target']].to_csv('submit.csv', index=None)
